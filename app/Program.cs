5e0d3d3fb194bbbda3f956ed6cb3d24f4fb10bd0
BenedettoBrusascaGit <ben2brusa@gmail.com>
2023-04-04 19:44:23 +0200

    Semaforo verde

--- app/Program.cs
+++ app/Program.cs
@@ -38,6 +38,19 @@ namespace BrusascaEdCivicaTIPSI
         {
             string alfabeto = "abcdefghijklmnopqrstuvwxyz";
             string risultato = "";
+            foreach (char c in s)
+            {
+                int posizione = alfabeto.IndexOf(c);
+                if (posizione >= 0)
+                {
+                    posizione = (posizione + n) % alfabeto.Length;
+                    risultato += alfabeto[posizione];
+                }
+                else
+                {
+                    risultato += c;
+                }
+            }
             return risultato;
         }  
     }
